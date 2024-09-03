41ad4578bd22d44789eb6db7e3c56c3f01924510
Akshay Mathad <138283175+akshay-mathad@users.noreply.github.com>
2024-09-03 22:00:07 +0530

    tittle

--- src/3-useState-array.js
+++ src/3-useState-array.js
@@ -5,24 +5,12 @@ import './3-useState-array.css'
   const UseStateArray = () => {
     const [people, setPeople] = useState(data);
   
-    const removeItem = (id) => {
-      let newPeople = people.filter((person) => person.id !== id);
-      setPeople(newPeople);
-    };
-  
   return (
     <>
     {people.map((person)=>{
       const {rank,image,bookTitle,author,rating,cover,price,link} = person;
       return (
         <divi>
-          {/* <h6>#{rank}</h6> */}
-          {/* <img src={image}></img> */}
-          {/* <h4>{title}</h4> */}
-          {/* <h2>{author}</h2> */}
-          {/* <h4>{rating}</h4> */}
-          {/* <h4>{price}</h4> */}
-          {/* <button onClick={() => removeItem(rank)}>remove</button> */}
 
           <book id="book">
             <rank id="rank" className="book">#{rank}</rank><br></br>
